d49f3e56c6642f8806426323f0ad295229fd6471
LeSangHiec <164978871+LeSangHiec@users.noreply.github.com>
2024-06-11 09:39:36 +0700

    quoc

--- Quoc2805/ntq-lesson06-hook/src/App.js
+++ Quoc2805/ntq-lesson06-hook/src/App.js
@@ -1,19 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 import NtqListTask from './components/NtqListTask';
 import NtqTaskAddOrEdit from './components/NtqTaskAddOrEdit';
 
 function App() {
   //Mock data
   const ntq_listTasks = [
-    { ntq_taskId: 1, ntq_taskName: "Học lập trình frontend", ntq_level: "Small" },
-    { ntq_taskId: 2, ntq_taskName: "Học lập trình reactjs", ntq_level: "Medium" },
-    { ntq_taskId: 3, ntq_taskName: "Lập trình với Frontend - Reactjs", ntq_level: "High" },
-    { ntq_taskId: 4, ntq_taskName: "Lập trình Fullstack (PHP, Java, NetCore)", ntq_level: "Small" },
-  ];
+    {
+      ntq_taskId: 2201234,
+      ntq_taskName: "Nguyễn Trung Quốc",
+      ntq_level: "Small",
+    },
+    {
+      ntq_taskId: 1,
+      ntq_taskName: "Học lập trình frontend",
+      ntq_level: "Small",
+    },
+    {
+      ntq_taskId: 2,
+      ntq_taskName: "Học lập trình reactjs",
+      ntq_level: "Medium",
+    },
+    {
+      ntq_taskId: 3,
+      ntq_taskName: "Lập trình với Frontend - Reactjs",
+      ntq_level: "High",
+    },
+    {
+      ntq_taskId: 4,
+      ntq_taskName: "Lập trình Fullstack (PHP, Java, NetCore)",
+      ntq_level: "Small",
+    },
+];
+
+let data = JSON.parse(localStorage.getItem("NtqK22CNT1DataTasks"));
+if (data === null || data.length === 0) {
+    data = ntq_listTasks;
+    localStorage.setItem("NtqK22CNT1DataTasks", JSON.stringify(data));
+}
+
+// sử dụng hàm useState để lưu trữ trạng thái dữ liệu
+const [ntqListTasks, setNtqListTasks] = useState(data);
+
+  
+useEffect(() => {
+    localStorage.setItem("NtqK22CNT1DataTasks", JSON.stringify(ntqListTasks));
+}, [ntqListTasks]);
+
   // sử dụng hàm useState để lưu trữ trạng thái dữ liệu
-  const [ntqListTasks, setNtqListTasks] = useState(ntq_listTasks);
   const ntqHandleSubmit = (ntqParam) => {
     if (ntqAddOrEdit === false) {
       // khi thêm mới
@@ -33,6 +68,10 @@ function App() {
     }
   };
   // ham xu ly đón dữ liệu sửa
+  const handleDelete = (taskId) => {
+    setNtqListTasks((prev) => prev.filter((task) => task.ntq_taskId !== taskId));
+  };
+
   // lữu trữ state
   const task = { ntq_taskId: 0, ntq_taskName: "HD", ntq_level: "small" };
   const [ntqTask, setNtqTask] = useState(task); // dùng để render form
@@ -49,7 +88,8 @@ function App() {
       <div>
         {/*Danh sach list task*/}
         <NtqListTask renderNtqListTasks={ntqListTasks}
-                     onNtqEdit={HandleEdit} />
+                     onNtqEdit={HandleEdit}
+                     onNtqDelete={handleDelete} />
       </div>
       <div>
         <NtqTaskAddOrEdit ntqOnSubmit={ntqHandleSubmit}
